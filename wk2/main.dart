03f96ba176a93c3a5f75b8bb7ec945679383cf65
Ian G. <iangaunt999@gmail.com>
2025-04-17 13:09:45 -0700

    Added selection sort

--- wk2/main.dart
+++ wk2/main.dart
@@ -1,6 +1,60 @@
+import 'dart:math';
+
 void main() {
-	
+    List<int> numbers = [];
+
+    for (int i = 0; i < 100; i++) {
+        numbers.add(Random.secure().nextInt(100));
+    }
+
+    print("\n\n\n");
+    print(numbers);
+    
+    for (int i = 0; i < numbers.length; i++) {
+        int smallest = -1;
+
+        for (int j = i; j < numbers.length; j++) {
+            if (smallest == -1 || numbers[j] < numbers[smallest]) {
+                smallest = j;
+            }
+        }
+
+        int temp = numbers[i];
+        numbers[i] = numbers[smallest];
+        numbers[smallest] = temp;
+    }
+
+    print(numbers);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
